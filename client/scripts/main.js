990c89afdb6ef977855f33c4fd8d20db9ca267db
carl.morner <carl.morner@limetta.se>
2014-08-04 17:31:03 +0200

    something

---
+++ client/scripts/main.js
@@ -0,0 +1,9 @@
+angular.module('App', [
+	'App.controllers'
+]);
+
+angular.module('App.controllers', []);
+
+angular.module('App.controllers', ['$scope', function ($scope) {
+	console.log('tjena');
+}]);
