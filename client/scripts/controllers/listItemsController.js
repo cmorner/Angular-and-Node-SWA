6233c94fc45b8d05514b50ca4da57a187bcba261
carl.morner <carl.morner@limetta.se>
2014-08-05 10:43:41 +0200

    Refactoring of listItemsController

---
+++ client/scripts/controllers/listItemsController.js
@@ -0,0 +1,31 @@
+angular.module('App.controllers')
+.controller('listItemsController', [
+	'$scope',
+	function ($scope) {
+		$scope.items = {
+			{
+				header: 'Målning dinosaurie',
+				img: 'imgUrl',
+				description: 'Akvarell målning gjord i färgglada färger',
+				artist: 'Anna Mörner'
+			},
+			{
+				header: 'Målning dinosaurie',
+				img: 'imgUrl',
+				description: 'Akvarell målning gjord i färgglada färger',
+				artist: 'Anna Mörner'
+			},
+			{
+				header: 'Målning dinosaurie',
+				img: 'imgUrl',
+				description: 'Akvarell målning gjord i färgglada färger',
+				artist: 'Anna Mörner'
+			},
+			{
+				header: 'Målning dinosaurie',
+				img: 'imgUrl',
+				description: 'Akvarell målning gjord i färgglada färger',
+				artist: 'Anna Mörner'
+			},
+		}
+	}]);
