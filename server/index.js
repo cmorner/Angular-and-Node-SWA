45dd7d86b566cf4ebae2f6b34760dc7d7900eb3b
carl.morner <carl.morner@limetta.se>
2014-08-29 16:56:38 +0200

    stuff

--- server/index.js
+++ server/index.js
@@ -2,16 +2,39 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser')
 
-app.get('/items', function (req, res) {
-	res.send('List items');
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.json());
+
+var port = process.env.PORT || 3000;
+
+var router = express.Router();
+
+
+app.set('title', 'SA');
+
+app.use(function (req, res, next) {
+	console.log(req.method + ' ' + req.url);
+	next();
 });
 
-app.get('/items', function (req, res) {
-	res.send('List items');
+
+// app.get('/', function (req, res) {
+// 	res.render('index');
+// });
+
+router.get('/items/:dynamic', function (req, res) {
+	res.json({ message: 'items:D' });
+	console.log('res:', res);
 });
 
-app.listen('3000');
-console.log('Server is up and running on port 3000');
+router.get('/item/', function (req, res) {
+	res.json({title: 'En vacker tafla'});
+});
+
+app.use('/api', router);
+
+app.listen(port);
+console.log('Server is up and running on port ', port);
 
 // POST /item/add
 // POST /item/edit
