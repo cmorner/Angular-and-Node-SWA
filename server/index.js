457594c0ba8194a80b873774ec95468612e5fc8d
carl.morner <carl.morner@limetta.se>
2014-08-29 15:59:22 +0200

    Alot of stuff :D

--- server/index.js
+++ server/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser')
 
-//app.get('/');
+app.get('/items', function (req, res) {
+	res.send('List items');
+});
+
+app.get('/items', function (req, res) {
+	res.send('List items');
+});
 
 app.listen('3000');
+console.log('Server is up and running on port 3000');
 
 // POST /item/add
 // POST /item/edit
